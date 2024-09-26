facbd55808c4e9eff8c825e8c94f0cc7de717710
Haris Skiadas <cskiadas@gmail.com>
2024-09-26 14:56:43 -0400

    fix linting

--- tests/test_ptx_space_at_inlined.py
+++ tests/test_ptx_space_at_inlined.py
@@ -8,13 +8,13 @@ class TestPtxSpacingAtInlines(ExpressionTestCase):
                        "<p>Hey there partner <c>Howdy</c></p>")
 
   def test_non_inlined_example_of_wrong_indent(self):
-    self.assertBecomes('''<p>
+    self.assertBecomes(
+        '''<p>
       This is really not a great class, but it will do as a first example. So what all do we have here?
       <ul>
         <li>
   A class is declared with the keyword `class`. It is followed by the name for the class, which is customarily in "upper camel case" form. Then the customary curly braces demarcate the contents of the class definition.
-    </li></ul></p>''',
-    '''<p>
+    </li></ul></p>''', '''<p>
   This is really not a great class, but it will do as a first example. So what all do we have here?
   <ul>
     <li>A class is declared with the keyword `class`. It is followed by the name for the class, which is customarily in "upper camel case" form. Then the customary curly braces demarcate the contents of the class definition.</li>
